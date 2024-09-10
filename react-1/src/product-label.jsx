b598ff312d5307e3ca8fb9b2d3bc3afde2006caf
Anil Yadav <rebelanil885@gmail.com>
2024-09-10 22:51:23 +0530

    new react

---
+++ react-1/src/product-label.jsx
@@ -0,0 +1,7 @@
+function product(){
+    return (<>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad qui molestiae iure!</p>
+    </>)
+}
+
+export default product;
---
+++ react-1/src/product.jsx
@@ -0,0 +1,9 @@
+import Product_label from "./product-label";
+
+function product (){
+    return (
+        <>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis labore cum non!</p>
+        </>
+    )
+}
