b355d0c296959b78d05f8b0059f6e3d80e828074
Anil Yadav <rebelanil885@gmail.com>
2024-09-18 00:17:48 +0530

    checking the server

--- basic-react/src/App.jsx
+++ basic-react/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import "./App.css";
+// import "./App.css";
 import Product from "./product";
 
 //All the components we made like Title and Boxes we don't make like this we create indivisual files for components as jsx form
