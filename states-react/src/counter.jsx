7605378edab21cc540ceb555abc2bed2d4436202
Anil Yadav <rebelanil885@gmail.com>
2024-09-27 23:39:36 +0530

    required useStates Hooks and used

--- states-react/src/counter.jsx
+++ states-react/src/counter.jsx
@@ -1,16 +1,23 @@
-let count = 0;
-function increaseCounter(){
-    count += 1;
-    console.log(count);
-}
+// let count = 0;
+// function increaseCounter(){
+//     count += 1;
+//     console.log(count);
+//     //here without using states this will not render to DOM only render to console so to render this we'll learn states in react
+// }
+import { useState } from "react"; //requiring useState hooks
 
-export default function (){
+export default function () {
+  //defining or initializing useState variable so that re-render work
+  let [count, setCount] = useState(0); //defined count variable and inialized as useState initialized so it will re-render to DOM let's see with again making function
+  let inCount = () => {
+    setCount(count + 1); //here setCount work for re-render while increment
+  };
 
-    return(
-        <>
-        <h2>States-in-js</h2>
-        <h3>{count}</h3>
-        <button onClick={increaseCounter}>Increase counter</button>
-        </>
-    )
+  return (
+    <>
+      <h2>States-in-js</h2>
+      <h3>{count}</h3>
+      <button onClick={inCount}>Increase counter</button>
+    </>
+  );
 }
