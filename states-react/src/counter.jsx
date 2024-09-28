cca17a87ec86d6d21e3ea44a49dc421f00e47118
Anil Yadav <rebelanil885@gmail.com>
2024-09-28 23:34:10 +0530

    uses of states basics

--- states-react/src/counter.jsx
+++ states-react/src/counter.jsx
@@ -10,7 +10,8 @@ export default function () {
   //defining or initializing useState variable so that re-render work
   let [count, setCount] = useState(0); //defined count variable and inialized as useState initialized so it will re-render to DOM let's see with again making function
   let inCount = () => {
-    setCount(count + 1); //here setCount work for re-render while increment
+    setCount(count + 1); //here setCount work for re-render while increment how while clicking each time this state setCount is like function each time increase it by one 
+    //it is not good way to using states but at beggining state it is just for what is the use of states
   };
 
   return (
