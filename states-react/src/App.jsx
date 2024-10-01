ad97d28174c0ebdd089117e613b3109ff4cace6b
Anil Yadav <rebelanil885@gmail.com>
2024-10-02 00:08:02 +0530

    liked full button

--- states-react/src/App.jsx
+++ states-react/src/App.jsx
@@ -7,10 +7,10 @@ function App() {
 
   return (
     <>
-      
-      <LikeBtn/>
+    <LikeBtn/>
     </>
   )
+  
 }
 
 export default App
--- states-react/src/LikeBtn.jsx
+++ states-react/src/LikeBtn.jsx
@@ -1,19 +1,24 @@
-import { useState } from "react"
+import { useState } from "react";
 
-export default function (){
-    let[like,setLiked] = useState(false);
-    let likeStyle = {width:"5rem",height:"3rem"};
-    let isLiked = () =>{
-      //while calling i want it should get opposite or not of it mean if btn is clicked while rendering it should be unliked 
-      setLiked(!like);
-    }
-    //Note:isliked or any these state method should only be within the export function block
-    return <>
-    (
-        <div>
-            <p>Liked button in states</p>
-            <p>Like <i class="fa-regular fa-heart" onClick={isLiked}>{isLiked.toString}</i></p>
-        </div>
-    )
+export default function () {
+  let [isLike, setIsLiked] = useState(false);
+  let likeStyle = { width: "5rem", height: "3rem" };
+  let toggleLike = () => {
+    //while calling i want it should get opposite or not of it mean if btn is clicked while rendering it should be unliked
+    setIsLiked(!isLike);
+    console.log("btn is going to change");
+  };
+  //Note:isliked or any these state method should only be within the export function block
+  return (
+    <>
+      <div>
+        <p>like button using states in react</p>
+        <p onClick={toggleLike}>
+            {
+                isLike ? <i class="fa-regular fa-heart"></i> : <i class="fa-solid fa-heart"></i> 
+            }
+        </p>
+      </div>
     </>
+  );
 }
