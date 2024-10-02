6d37e033c4347cb785331af150268d918f5230bd
Anil Yadav <129471687+anilyadav45@users.noreply.github.com>
2024-10-02 23:16:41 +0530

    Update LikeBtn.jsx
    like styles

--- states-react/src/LikeBtn.jsx
+++ states-react/src/LikeBtn.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 export default function () {
   let [isLike, setIsLiked] = useState(false);
-  let likeStyle = { width: "5rem", height: "3rem" };
+  let likeStyle = { width: "5rem", height: "3rem" ,color:"red"};
   let toggleLike = () => {
     //while calling i want it should get opposite or not of it mean if btn is clicked while rendering it should be unliked
     setIsLiked(!isLike);
@@ -15,7 +15,7 @@ export default function () {
         <p>like button using states in react</p>
         <p onClick={toggleLike}>
             {
-                isLike ? <i class="fa-regular fa-heart"></i> : <i class="fa-solid fa-heart"></i> 
+                isLike ? <i class="fa-regular fa-heart"></i> : <i class="fa-solid fa-heart" styles={likeStyle}></i> 
             }
         </p>
       </div>
