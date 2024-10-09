68f552ec8e175be76884210a8eda6aa1004e70e5
Anil Yadav <rebelanil885@gmail.com>
2024-10-09 23:43:01 +0530

    faced some errors while passing dynamic imgs data

--- basic-react/src/product.jsx
+++ basic-react/src/product.jsx
@@ -3,13 +3,14 @@ import Price from "./price";
 import Desc from "./desc";
 import "./product.css";
 import Button from "./button.jsx";
-function product({ title, desc, prices}) {
+function product({ title, desc, prices,imageSrc}) {
     let idx = 0;
     let styles = {display:"flex",width:300,justifyContent:"space-around" ,flexDirection:"column" ,backgroundColor:"#3f8cd4",alignItems:"center",height:"200px"};
   return (
     <>
       <div className="product" style={styles}>
         <Title title={title} />
+        <img src={imageSrc} alt={title} className="product-image" /> {/* Render image */}
         <Desc desc={desc} />
         <Price prices={prices} />
         <Button/>
--- basic-react/src/productTab.jsx
+++ basic-react/src/productTab.jsx
@@ -1,16 +1,43 @@
 import Product from "./product.jsx";
 import "./productTab.css";
+//importing images dynamically
+import image1 from "../assets/intex.webp"; // Import images
+import image2 from "../assets/lapcare.webp";
+import image3 from "../assets/shopping.webp";
+import image4 from "../assets/speaker.webp";
 
-function productTab(){
-    return (
-        <>
-        <div className="productTab">
-            <Product title=" LogiTech | Mx Destro " desc="500 DPI and programmable" prices={[90999,80900]}/>
-            <Product title=" LapCare | CoolingPad " desc="Super Cooling Fan with 20rpm" prices={[1119,420]}/>
-            <Product title=" IntexSoft | Mouse" desc="400 DPI and programmable" prices={[2339,800]}/>
-            <Product title=" MozesticLara | Speakers " desc="500Hzs surroundings audio" prices={[1999,900]}/>
-        </div>
-        </>
-    )
+console.log(image1); // Add this line in your productTab component to see if the path resolves.
+function productTab() {
+
+  return (
+    <>
+      <div className="productTab">
+        <Product
+          title=" LogiTech | Mx Destro "
+          desc="500 DPI and programmable"
+          prices={[90999, 80900]}
+          imageSrc={image1} // Pass image prop
+        />
+        <Product
+          title=" LapCare | CoolingPad "
+          desc="Super Cooling Fan with 20rpm"
+          prices={[1119, 420]}
+          imageSrc={image2} // Pass image prop
+        />
+        <Product
+          title=" IntexSoft | Mouse"
+          desc="400 DPI and programmable"
+          prices={[2339, 800]}
+          imageSrc={image3} // Pass image prop
+        />
+        <Product
+          title=" MozesticLara | Speakers "
+          desc="500Hzs surroundings audio"
+          prices={[1999, 900]}
+          imageSrc={image4} // Pass image prop
+        />
+      </div>
+    </>
+  );
 }
 export default productTab;
